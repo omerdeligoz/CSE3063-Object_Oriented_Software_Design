76e23dc061ef7285740a7dc6f3be1192d417ab03
omerdeligoz <deligozomer@hotmail.com>
2023-11-17 00:14:19 +0300

    added Enes's comments

--- iteration1/Lecturer.java
+++ iteration1/Lecturer.java
@@ -7,22 +7,24 @@ public class Lecturer extends Person {
 
     private List<Course> lessonsTaught;
 
-    //For creating lecturer
+    //For creating lecturer with person abstract class.
     public Lecturer(int ID, String name, String surname) {
         super(ID, name, surname, null,null);
         lessonsTaught = new ArrayList<>();
     }
 
-    //For creating advisor
+    //For creating advisor with person abstract class.
     public Lecturer(int ID, String name, String surname, String userName, String password) {
         super(ID, name, surname, userName, password);
         lessonsTaught = new ArrayList<>();
     }
 
+    //Get the given courses lecturer gives.
     public List<Course> getLessonsTaught() {
         return lessonsTaught;
     }
 
+    //Advisor login proccess.
     @Override
     public boolean login(String userName, String password) {
         return false;
