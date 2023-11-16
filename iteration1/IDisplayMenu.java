41e8abb4b600d6e3473ea3c79fe554280bd53f26
omerdeligoz <deligozomer@hotmail.com>
2023-11-17 00:08:47 +0300

    added Mehrin's comments

--- iteration1/IDisplayMenu.java
+++ iteration1/IDisplayMenu.java
@@ -1,5 +1,12 @@
 package iteration1;
-
+/**
+ * This interface is used to display menus.
+ */
 public interface IDisplayMenu {
+    /**
+     * Prints the menu for the specified menu type.
+     *
+     * @param menuType The type of menu to print.
+     */
     void printMenu(String menuType);
 }
