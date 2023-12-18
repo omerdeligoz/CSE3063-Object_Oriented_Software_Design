303b873c3d3a3a6fe06298b52a920ffa98bbe586
omerdeligoz <deligozomer@hotmail.com>
2023-12-18 20:12:46 +0300

    labSections updated

--- iteration2/LaboratorySection.java
+++ iteration2/LaboratorySection.java
@@ -2,26 +2,23 @@ package iteration2;
 
 /**
  * The LaboratorySection class represents a course with a laboratory offered by an educational institution in different departments.
- * It extends course class.
- * It contains extra information about the laboratorySectionCode, capacity, assistant and students of the section.
+ * It contains extra information about the laboratorySectionCode, capacity, assistant and numberOfStudents,hour,day of the section.
  */
 public class LaboratorySection {
     private String laboratorySectionCode;
-    private Assistant assistant;
-
     private int capacity;
-
+    private Assistant assistant;
     private int numberOfStudents;
-
     private int hour;
     private String day;
 
     /**
      * Creates a new LaboratorySection object with the given parameters
-     * Calls the course class's constructor
      *
      * @param laboratorySectionCode the section's code
      * @param capacity              the section's capacity
+     * @param hour                  the section's hour
+     * @param day                   the section's day
      */
 
     public LaboratorySection(String laboratorySectionCode, int capacity, int hour, String day) {
