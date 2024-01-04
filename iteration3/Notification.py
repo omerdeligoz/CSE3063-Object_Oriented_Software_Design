cde36353e493a1fc0a3e45f4c4050f3f686e9021
omerdeligoz <deligozomer@hotmail.com>
2024-01-05 00:37:06 +0300

    polymorphism used

--- iteration3/Notification.py
+++ iteration3/Notification.py
@@ -1,8 +1,3 @@
-import logging
-
-from iteration3.Advisor import Advisor
-from iteration3.Student import Student
-
 
 class Notification:
     def __init__(self, recipient, message):
@@ -10,11 +5,7 @@ class Notification:
         self.message = message
 
     def sendNotification(self):
-        if isinstance(self.recipient, Advisor):
-            self.recipient.setNotification(self)
-        elif isinstance(self.recipient, Student):
-            logging.info(f"Notification sent to student {self.recipient.getID()} that their request has been rejected.")
-            self.recipient.setNotification(self)
+        self.recipient.setNotification(self)
 
     def getMessage(self):
         return self.message
